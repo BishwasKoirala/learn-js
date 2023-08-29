0aeaf849a1d26ca5904d0b9821df040e7a5c2d45
Bishwas Koirala <wasubisu69@gmail.com>
2023-08-29 23:18:01 +0900

    next solve some problems

--- 11_arrays_loops/todo-list.js
+++ 11_arrays_loops/todo-list.js
@@ -1,4 +1,22 @@
-const todoList = [];
+const todoList = ['make dinner', 'wash dishes'];
+
+rendertodoList()
+
+function rendertodoList(){
+
+  let todoListHTML = ""
+
+  for (let i = 0; i < todoList.length; i++) {
+    const todo = todoList[i];
+    const html = `<p>${todo}<p/>`
+    todoListHTML += html
+  }
+
+  console.log(todoListHTML);
+
+  document.querySelector('.js-todo-list')
+  .innerHTML = todoListHTML
+}
 
 function addTodo() {
   const inputElement = document.querySelector(".js-name-input");
@@ -10,7 +28,7 @@ function addTodo() {
   
   inputElement.value = "";
 
-  displayText()
+  rendertodoList();
 }
 
 
