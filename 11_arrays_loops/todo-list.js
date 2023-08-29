3b8225966410cfc410877bdcbcd882007e88fc13
Bishwas Koirala <wasubisu69@gmail.com>
2023-08-29 22:21:48 +0900

    learned about for loops and how i can loop through arrays

--- 11_arrays_loops/todo-list.js
+++ 11_arrays_loops/todo-list.js
@@ -2,13 +2,16 @@ const todoList = [];
 
 function addTodo() {
   const inputElement = document.querySelector(".js-name-input");
-
   const name = inputElement.value;
+
   todoList.push(name);
   console.log(todoList);
 
   
   inputElement.value = "";
+
+  displayText()
 }
+
 
 // next display each vale on the page
