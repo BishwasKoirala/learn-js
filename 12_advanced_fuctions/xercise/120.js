8e92e7ee8fd61d15c9315da313115ea0864b943a
wasub <wasubisu69@gmail.com>
2023-09-22 19:02:56 +0900

    wsl test

--- 12_advanced_fuctions/xercise/120.js
+++ 12_advanced_fuctions/xercise/120.js
@@ -3,15 +3,15 @@ function removeEggs(food, egg) {
   let result = [];
   for (let i of food) {
     if (i === egg && counter < 2) {
-      counter++
+      counter++;
     } else {
-      result.push(i)
+      result.push(i);
     }
   }
   return result;
 }
 
-let foods = ["egg", "cat", "meat","egg","egg","egg"];
+let foods = ["egg", "cat", "meat", "egg", "egg", "egg"];
 let eggs = "egg";
 
 console.log(removeEggs(foods, eggs));
