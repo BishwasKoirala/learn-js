66292550748db6164478455c92d41371adfdc5de
Bishwas Koirala <wasubisu69@gmail.com>
2023-09-29 16:25:44 +0900

    aksd

--- 12_advanced_fuctions/x2/12l.js
+++ 12_advanced_fuctions/x2/12l.js
@@ -1,13 +1,11 @@
 const countPositiveNums = (nums) => {
   let positiveNumbers = 0;
-    nums.forEach(element => {
-      if (element >= 0) {
+    nums.forEach(num => {
+      if (num >= 0) {
         positiveNumbers++;
-      } else {
-        return;
       }
 
-      })
+    })
   return positiveNumbers;
 };
 const list = [1, 2, 3, 6, -1, -2, -3];
