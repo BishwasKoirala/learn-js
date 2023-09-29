e97bd36504d316dc8d9b32575731d8858518eb91
Bishwas Koirala <wasubisu69@gmail.com>
2023-09-29 21:25:11 +0900

    sdfsd

--- 12_advanced_fuctions/x2/12m.js
+++ 12_advanced_fuctions/x2/12m.js
@@ -11,11 +11,10 @@ let num1 = 2;
 
 // console.log(AddNum(array1, num1));
 
-
 // dowith for each
-function addNum(array,num) {
+function addNum(array, num) {
   let result = array.map((value) => value + num);
   return result;
-};
+}
 
-console.log(addNum(array1,num1))
+console.log(addNum(array1, num1));
