c8d4fac5e07a99842098610761f4b5d7b46b50d3
Bishwas Koirala <wasubisu69@gmail.com>
2023-10-01 01:16:50 +0900

    do12o again

--- 12_advanced_fuctions/x2/12o.js
+++ 12_advanced_fuctions/x2/12o.js
@@ -2,15 +2,15 @@ function removeEggs(foods, toRemove) {
   let counter = 0;
   return foods.filter((food) => {
     if (counter < 2) {
-      food !== toRemove;
+      return food !== toRemove;
       counter ++
     } else {
-      food !== toRemove
+      return food !== toRemove
     }
   });
 }
 
-const theFoods = ["apple", "ball", "eggs", "cat", "doggs", "eggs"];
+const theFoods = ["apple", "ball", "eggs", "cat", "doggs", "eggs",'birds','eggs',,'eggs'];
 
 const theString = "eggs";
 
