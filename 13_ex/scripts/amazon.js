12b2ccc7681043a60231c3345b5725ed28f8ae74
BishwasKoirala <wasubisu69@gmail.com>
2023-10-10 18:15:19 +0900

    commiting exercises

--- 13_ex/scripts/amazon.js
+++ 13_ex/scripts/amazon.js
@@ -41,7 +41,7 @@ products.forEach((product) => {
 
       <div class="product-spacer"></div>
 
-      <div class="added-to-cart">
+      <div class="added-to-cart js-added-to-cart-${product.id}">
         <img src="images/icons/checkmark.png">
         Added
       </div>
@@ -59,7 +59,7 @@ document.querySelector(".js-products-grid").innerHTML = productsHTML
 
 document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
   button.addEventListener('click',() => {
-    const productId = button.dataset.productId;
+    const {productId} = button.dataset;
 
     let matchingItem ;
 
@@ -79,8 +79,8 @@ document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
       matchingItem.quantity += quantity;
     } else {
       cart.push({
-        productId: productId,
-        quantity : quantity
+        productId,
+        quantity
       });
     }
 
@@ -90,5 +90,15 @@ document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
     })
 
     document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+
+    const addedMessage = document.querySelector(
+      `.js-added-to-cart-${productId}`
+    );
+    addedMessage.classList.add('added-to-cart-visible');
+
+    setTimeout(()=>{
+      
+      addedMessage.classList.remove('added-to-cart-visible')
+    },2000)
   });
 });
